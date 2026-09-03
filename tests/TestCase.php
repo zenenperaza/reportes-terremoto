4568f5d3e50dac72ae1b3cdfe7f3068389e92dbb
Zenen Peraza <peraza@outlook.com>
2026-09-03 13:54:12 -0400

    cambios

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use LogicException;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication(): Application
+    {
+        $app = parent::createApplication();
+        $connection = $app['config']->get('database.default');
+        $database = $app['config']->get("database.connections.{$connection}.database");
+
+        if ($connection !== 'sqlite' || $database !== ':memory:') {
+            throw new LogicException(
+                'Pruebas canceladas: la conexión debe ser SQLite en memoria. Ejecute php artisan optimize:clear antes de PHPUnit.'
+            );
+        }
+
+        return $app;
+    }
 }
